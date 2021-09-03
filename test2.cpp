85399e9a494e7fa28524a316ad8d3832847b3b4c
gultz <gultz5102@kakao.com>
2021-09-03 18:12:48 +0900

    first commit

---
+++ test2.cpp
@@ -0,0 +1,19 @@
+include <iostream>
+using namespace std;
+
+int check(int c){
+
+    if(c==3){
+        return 10;
+    }
+    int temp =100;
+    return temp;
+
+}
+
+int main(){
+
+    int temp =check(3);
+    cout<<temp<<'\n';
+
+}
